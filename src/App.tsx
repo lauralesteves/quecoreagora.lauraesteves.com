e59e29ffa83419aa7e32b2543231bebfa89b6c76
Laura Esteves <git@lauraesteves.com>
2026-03-10 14:58:08 -0300

    feat: add GitHub corner with octocat wave animation
    White SVG octocat in top-right corner linking to the repo.
    Arm waves on hover (desktop) and on load (mobile).
    Based on tholman/github-corners.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ColorDisplay } from './components/ColorDisplay/ColorDisplay';
 import { Footer } from './components/Footer/Footer';
+import { GithubCorner } from './components/GithubCorner/GithubCorner';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="flex flex-col h-dvh">
+      <GithubCorner />
       <Navbar />
       <ColorDisplay />
       <Footer />
